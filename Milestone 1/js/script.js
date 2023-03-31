bc5374648fcec6f93b0b98dda2e2aa9cb62d016c
Fedekh <federicocet@gmail.com>
2023-03-31 16:27:38 +0200

    i started late, i tried to do the first three blocks to left as beast as possibile, maybe later i changes somethings

--- Milestone 1/js/script.js
+++ Milestone 1/js/script.js
@@ -3,7 +3,7 @@
 // Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi)
  //e dall’interlocutore (bianco) assegnando due classi CSS diverse
 // Visualizzazione dinamica della lista contatti: tramite la direttiva v-for,
-//visualizzare nome e immagine di ogni contatto.
+// visualizzare nome e immagine di ogni contatto.
 // Consigli del giorno:
 // - scegliete con attenzione gli strumenti che utilizzerete;
 
