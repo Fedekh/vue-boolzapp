8db6923ff7dc21fb8604528308990c30bc4af44b
Fedekh <federicocet@gmail.com>
2023-03-31 12:15:42 +0200

    insert first try of layout

---
+++ Milestone 1/js/script.js
@@ -0,0 +1,25 @@
+// Oggi iniziamo a realizzare Boolzapp 
+// Milestone 1:
+// Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi)
+ //e dall’interlocutore (bianco) assegnando due classi CSS diverse
+// Visualizzazione dinamica della lista contatti: tramite la direttiva v-for,
+//visualizzare nome e immagine di ogni contatto.
+// Consigli del giorno:
+// - scegliete con attenzione gli strumenti che utilizzerete;
+
+// - tenete in mente le funzionalità che dovranno essere implementate nei giorni successive.
+//  La struttura realizzata mi faciliterà rendere il tutto dinamico? 
+//Forse mi serve una classe per distinguere i messaggi innviati da quelli ricevuti?
+
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return{
+
+        }
+    },
+    methods:{
+
+    }
+}).mount("#app");
