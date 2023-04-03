681aec965c7652a4f1419c77ef56aff3a902aa30
Fedekh <federicocet@gmail.com>
2023-04-03 17:27:28 +0200

    complete all milestone 5

--- Milestone 5/js/script.js
+++ Milestone 5/js/script.js
@@ -17,17 +17,20 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Ricordati di stendere i panni',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             message: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         }
                     ],
                 },
@@ -39,17 +42,20 @@ createApp({
                         {
                             date: '20/03/2020 16:30:00',
                             message: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             message: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         }
                     ],
                 },
@@ -61,17 +67,20 @@ createApp({
                         {
                             date: '28/03/2020 10:10:40',
                             message: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             message: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             message: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         }
                     ],
                 },
@@ -83,12 +92,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         }
                     ],
                 },
@@ -100,12 +111,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ricordati di chiamare la nonna',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Va bene, stasera la sento',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         }
                     ],
                 },
@@ -117,17 +130,20 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao Claudia, hai novità?',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Non ancora',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'Nessuna nuova, buona nuova',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         }
                     ],
                 },
@@ -139,12 +155,14 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         }
                     ],
                 },
@@ -156,17 +174,20 @@ createApp({
                         {
                             date: '10/01/2020 15:30:55',
                             message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                            status: 'sent'
+                            status: 'sent',
+                            showMenu: false
                         },
                         {
                             date: '10/01/2020 15:51:00',
                             message: 'OK!!',
-                            status: 'received'
+                            status: 'received',
+                            showMenu: false
                         }
                     ],
                 }
@@ -176,8 +197,6 @@ createApp({
             currentMessage: "", //selezioniamo l'attuale emssaggio in main window
             newMessage: "",         //ci scriveremo un nuovo messaggio dopo aver aperto una chat
             filterFriend: "",   //ci servirà per filtrare un amico nella ricerca
-            hover: false        //indica se ci sono passato sopra col mouse o meno
-
         }
     },
 
@@ -210,8 +229,7 @@ createApp({
             }
         },
 
-
-        //proviamo ad aggiungere effetto hover al click e rimuoverlo se ne clicco un altro
+        //proviamo ad aggiungere effetto hover al click chat sx e rimuoverlo se ne clicco un altro
         toggleHoverClass(currentElemento) {
             this.contacts.forEach(elemento => {
                 if (elemento !== currentElemento) {
@@ -236,10 +254,25 @@ createApp({
             }
         },
 
-        //proviamo a cancellare un messaggio
-        setHover(value) {
-            this.hover = value
+        //proviamo a creare un menu a tendina e ho inserito unl'atra key
+        //all interno dell array di oggetti messages settandolo a
+        toggleMenu(elemento) {
+            elemento.showMenu = !elemento.showMenu;
           },
+
+        //proviamo a far si che se vado via dall hover e ritorno, non parta da solo
+        //il minimenu
+        hideMenu(elemento) {
+            if (elemento.showMenu) {
+                elemento.showMenu = false;
+            }
+        },
+
+        //proviamo a cancellare un messaggio
+        deleteMessage(elemento) {
+            const index = this.currentMessage.indexOf(elemento);
+            this.currentMessage.splice(index, 1);
+        },
        
           
     }
